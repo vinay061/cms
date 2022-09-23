5a86d107c4c47e45b6f7a408b3cf862d7b4c0ca1
Vinay <vinay061@gmail.com>
2022-09-24 00:00:08 +0200

    update db

--- keystone.ts
+++ keystone.ts
@@ -19,8 +19,8 @@ export default withAuth(
   config({
     // the db sets the database provider - we're using sqlite for the fastest startup experience
     db: {
-      provider: 'sqlite',
-      url: 'file:./keystone.db',
+      provider: 'postgresql',
+      url: 'postgres://vinayapp@localhost/railway',
     },
     // This config allows us to set up features of the Admin UI https://keystonejs.com/docs/apis/config#ui
     ui: {
